d725e072bd8743e4d16f66b75752338723b47388
Jan Brecka <jan.brecka@gmail.com>
2021-08-04 10:53:15 +0200

    improve parser

--- index.js
+++ index.js
@@ -1,85 +1,103 @@
 #!/usr/bin/env node
 
-const { promisify } = require('util')
-const { pipeline, Transform } = require('stream')
-const es = require('event-stream')
+const { promisify } = require("util");
+const { pipeline, Transform } = require("stream");
+const es = require("event-stream");
 
-const pipe = promisify(pipeline)
+const pipe = promisify(pipeline);
+
+const parseLine = (s) => {
+  const [, key, value] = s.split("│");
+  return [key, value].map((s) => s.trim());
+};
+
+const parseBlock = (block) =>
+  block
+    .map((row) => row.flatMap(parseLine))
+    .map(([key, ...vals]) => [key, vals.join(" ")]);
 
 class Parser extends Transform {
   constructor() {
-    super({ objectMode: true })
-    this.inside = false
-    this.block = []
+    super({ objectMode: true });
+    this.row = [];
+    this.block = [];
   }
-  _transform (chunk, _, done) {
-    const line = chunk.toString()
+  _transform(chunk, _, done) {
+    const line = chunk.toString();
 
-    if (line.indexOf('└') > -1) {
-      this.push(this.block)
-      this.block = []
-      this.inside = false
+    if (line.indexOf("┌") > -1) {
+      this.block = [];
+      this.row = [];
     }
 
-    if (this.inside) this.block.push(line)
-
-    if (line.indexOf('┌') > -1) {
-      this.inside = true
+    if (line.indexOf("└") > -1) {
+      this.block.push(this.row);
+      this.push(parseBlock(this.block));
     }
 
-    done()
+    if (line.indexOf("├") > -1) {
+      this.block.push([...this.row]);
+      this.row = [];
+    }
+
+    if (line.indexOf("│") > -1) {
+      this.row.push(line);
+    }
+
+    done();
   }
 }
 
 class UniqueBy extends Transform {
   constructor(by) {
-    super({ objectMode: true })
-    this.by = by
-    this.seen = new Set()
+    super({ objectMode: true });
+    this.by = by;
+    this.seen = new Set();
   }
-  _transform (chunk, _, done) {
-    const hash = this.by(chunk)
+  _transform(chunk, _, done) {
+    const hash = this.by(chunk);
     if (!this.seen.has(hash)) {
-      this.seen.add(hash)
-      this.push(chunk)
+      this.seen.add(hash);
+      this.push(chunk);
     }
-    done()
+    done();
   }
 }
 
 const removeColors = (s) =>
-  s.replace(/[\u001b\u009b][[()#;?]*(?:[0-9]{1,4}(?:;[0-9]{0,4})*)?[0-9A-ORZcf-nqry=><]/g, '')
+  s.replace(
+    /[\u001b\u009b][[()#;?]*(?:[0-9]{1,4}(?:;[0-9]{0,4})*)?[0-9A-ORZcf-nqry=><]/g,
+    ""
+  );
 
-const parseLine = (s) => {
-  const [, key, value] = s.split('│')
-  return [key, value].map((s) => s.trim())
-}
-
-const csvRow = (...cols) => cols.join(';')
+const csvRow = (...cols) => cols.join(";");
 
 async function run(cwd) {
-  const { dependencies = {} } = require(`${cwd}/package.json`)
-  const dependenciesToCheck = new Set(Object.keys(dependencies))
-  console.log(csvRow('package', 'severity', 'reason', 'patchedIn'))
+  const { dependencies = {} } = require(`${cwd}/package.json`);
+  const dependenciesToCheck = new Set(Object.keys(dependencies));
+  console.log(csvRow("package", "severity", "reason", "patchedIn"));
   await pipe(
     process.stdin,
     es.split(),
     es.mapSync(removeColors),
     new Parser(),
-    es.mapSync(([reasonLine,, packageLine,, patchedInLine,, dependencyLine]) => {
-      const [severity, reason] = parseLine(reasonLine)
-      const [, package] = parseLine(packageLine)
-      const [, patchedIn] = parseLine(patchedInLine)
-      const [, dependency] = parseLine(dependencyLine)
-      return { severity, reason, package, patchedIn, dependency }
-    }),
-    es.filterSync(({ dependency }) => dependenciesToCheck.has(dependency)),
-    new UniqueBy(({ package, reason }) => `${package}/${reason}`),
-    es.mapSync(({ package, severity, reason, patchedIn }) =>
-      `${csvRow(package, severity, reason, patchedIn)}\n`
+    es.mapSync(
+      ([[severity, reason], [, package], [, patchedIn], [, dependency]]) => ({
+        severity,
+        reason,
+        package,
+        patchedIn,
+        dependency,
+      })
+    ),
+    es.filterSync(({ dependency }) => dependenciesToCheck.has(dependency)),
+    new UniqueBy(({ package, reason }) => `${package}/${reason}`),
+    es.mapSync(
+      ({ package, severity, reason, patchedIn }) =>
+        `${csvRow(package, severity, reason, patchedIn)}\n`
     ),
     process.stdout
-  )
+  );
 }
 
-run(process.cwd())
+run(process.cwd());
